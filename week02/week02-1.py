f3dc9044bc34fd72451133f45e6fa851c12b9fb1
enyashishi <enyatseng46@gmail.com>
2024-03-01 11:11:27 +0800

    week02

---
+++ week02/week02-1.py
@@ -0,0 +1,8 @@
+# (+程式碼) Python 對應的版本
+print("請輸入兩個整數:")
+a,b=list(map(int,input().split()))
+print("你輸入2個數字，直式加法:")
+print(f'{a:5}') # 使用 f'字串' 大括號 
+print(f'{b:5}')
+print('-------')
+print(f'{a+b:5}')
