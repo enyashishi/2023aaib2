f0885531cc989eeda11c3b78d5117bc768b3e54b
enyashishi <enyatseng46@gmail.com>
2024-03-15 10:24:58 +0800

    weel04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=1;i<=9;i++){
+        printf("i是%d",i);
+    }
+    printf("\n"); ///跳行
+    for(int i=9;i>=1;i--){
+        printf("i是%d",i);
+    }
+}
