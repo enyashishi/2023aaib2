c8eefb4436055daaa88ccd5e2d915fbde0ca756f
enyashishi <enyatseng46@gmail.com>
2024-03-29 10:03:55 +0800

    week06

---
+++ week06/week06-1b.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+
+    for(int i=1;i<10;i++){ ///左手i
+        for(int j=1;j<=n;j++){ ///右手j
+            printf("*"); ///全部印星星
+        }
+        printf("i;%d\n",i); ///第i樓
+    }
+}
